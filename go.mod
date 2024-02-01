0e966925913b4ed4e2d7c5390b13da10c62e9b11
alwan <mardika200@gmail.com>
2024-02-01 08:52:05 +0700

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module TestGolang
+
+go 1.21.6
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/lib/pq v1.10.9
+	golang.org/x/crypto v0.18.0
+)
---
+++ pkg/store/sqlstore/postgres/postgres.go
@@ -0,0 +1,14 @@
+package postgres
+
+import "database/sql"
+
+type postgresStore struct{
+	db *sql.DB
+}
+
+func NewPostgresStore(db *sql.DB) *postgresStore {
+	return &postgresStore{
+		db:db,
+	}
+	
+}
